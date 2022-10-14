4dfdd07fd2c3c250c4c7286db77cd62fd49c5ab3
Angel Moreno <113930724+Amoreno73@users.noreply.github.com>
2022-10-14 17:45:00 +0000

    final push it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from random import randint
-from random import shuffle
-from random import choice
+from random import randint, shuffle, choice
+# from random import shuffle
+# from random import choice
 
   # random
   # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
@@ -44,3 +44,21 @@ for num in range(2,201):
 color = ["red","blue","pink","purple"]
 random_color = choice(color)
 print(f"random color is {random_color}")
+
+  ################################################random in python#################################################
+  # Random Practice #1
+  # Implement the randint() function from the random library that allows you to obtain an integer from 1 to 10, and store that value in a variable called random_number.
+random_number = randint(1,10)
+print(random_number)
+  
+  # Random Practice #2
+  # Implement the random() function from the random library to obtain a real number between 0 and 1, and store that value in a variable called random_number.
+random_number = randint(0,1)
+print(random_number)
+  
+  # Random Practice #3
+  # Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+random_name = choice(names)
+print(random_name)
+
