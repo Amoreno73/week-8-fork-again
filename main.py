b9a6bd06ab8c8fcde77a5b24db6dc8278e814dd0
Angel Moreno <113930724+Amoreno73@users.noreply.github.com>
2022-10-14 17:14:27 +0000

    if else statements and for loop to find even and odd numbers

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from random import randint
-  
+from random import shuffle
+
   # random
   # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
   #show you two useful functions for now.
-print("random")
-dice1 = randint(1,7)
-print(f"dice1: {dice1}")
-dice2 = randint(1,7)
-print(f"dice2: {dice2}")
-rolledDoubles = dice1 == dice2
-if rolledDoubles:
-  print("you rolled doubles")
-else: print("not doubles")
 
-if dice1 and dice2 == 1:
-  print("you rolled snake eyes")
+# print("random")
+# dice1 = randint(1,7)
+# print(f"dice1: {dice1}")
+# dice2 = randint(1,7)
+# print(f"dice2: {dice2}")
+# rolledDoubles = dice1 == dice2
+# if rolledDoubles:
+#   print("you rolled doubles")
+# else: print("not doubles")
+
+# if dice1 and dice2 == 1:
+#   print("you rolled snake eyes")
+# else: print("you did not roll snake eyes")
+
+
+# practice with shuffle
+my_list = range(1,51)
+print("my new list")
+print(list(my_list))
+my_list = list(my_list) #redefining variable
+shuffle(my_list)
+print(my_list)
+
+random = randint(2,201)
+print("random num from 1-200: " + str(random))
+
+if random % 2 == 0:
+  print('even number')
+else:
+  print('odd number')
+
+for num in range(2,201):
+  if num % 2 == 0:
+    print(num)
